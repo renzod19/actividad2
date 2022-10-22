14da2f8e53ec71e55ac370c2e97c6ad65c3258e6
renzod19 <116091366+renzod19@users.noreply.github.com>
2022-10-22 18:36:25 +0200

    Hello World

---
+++ actividad2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_world : MonoBehaviour
+{
+    public string message;
+
+    void Update() {
+        print(message);
+        
+    }
+}
